3bc8c9bf129c6be563822114046c2a745d063ae1
lchaney <l2chaney@gmail.com>
2016-01-08 12:09:32 -0700

    changed color histogram

--- refined_stats.R
+++ refined_stats.R
@@ -29,14 +29,15 @@ max(unmap$ContigLength) #maximum length of contigs
 unmap$cum_sum <- cumsum(unmap$ContigLength)
 
 # Where is N50?
-unmap[unmap$cum_sum > max(unmap$cum_sum)/2,][1,]
+unmap[unmap$cum_sum > max(unmap$cum_sum)/2,][1,2]
 
 
 #similar plot as Crocker Fig 2
 library(ggplot2)
 
 ggplot(data = unmap, aes(ContigLength)) + 
-  geom_histogram() +
+  geom_histogram(fill = "darkblue") +
   scale_x_continuous(labels = function(x) x/1000) + #convert bp to kb for x
   labs(x = "Length of Consesus Maps (KB)", #x axis label
-       y = "Genome Map Number") #y axis label
+       y = "Genome Map Number") + #y axis label
+  theme_bw()
